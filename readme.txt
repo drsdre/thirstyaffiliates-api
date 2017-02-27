2c1c08280cf79931110522a7341cd1286f58df8c
Drsdre <github.com@drsdre.org>
2017-02-27 16:18:31 +0100

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== ThirstyAffiliates API ===
+Contributors: drsdre
+Tags: comments, spam
+Requires at least: 3.7
+Tested up to: 4.7.2
+Stable tag: 0.1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Enable WP Core Rest API v2 for ThirstyAffiliates data
+
+== Description ==
+
+This plugin enables access to ThirstyAffiliates data through the WordPress Rest API v2 (part of WP core since 4.7).
+
+Two new endpoints will be available:
+
+1. An end point to read/update the ThirstyAffiliate links on /wp-json/wp/v2/thirstylink
+
+2. An end point to read/update the ThirstyAffiliate categories on /wp-json/wp/v2/thirstylink-category
+
+== Installation ==
+
+1.  Extract the zip file and copy contents in the wp-content/plugins/ directory of your WordPress installation
+2.  Login as admin and go to Plugins
+3.  Activate this plugin
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 1.0 =
+* initial release
+
+== Upgrade Notice ==
+
+= 1.0 =
+None.
---
+++ thirstyaffiliates-api.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Plugin Name:     ThirstyAffiliates API
+ * Plugin URI:      https://github.com/drsdre
+ * Description:     Enable WP Core Rest API v2 for ThirstyAffiliates data
+ * Author:          Andre Schuurman
+ * Author URI:      https://github.com/drsdre
+ * Text Domain:     thirstyaffiliates-api
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         thirstyaffiliates-api
+ */
+
+/**
+ * Add REST API support to thirstylink post type.
+ */
+add_action( 'init', 'thirstylink_post_enable_rest_api', 25 );
+function thirstylink_post_enable_rest_api() {
+	global $wp_post_types;
+
+	$post_type_name = 'thirstylink';
+	if ( isset( $wp_post_types[ $post_type_name ] ) ) {
+		$wp_post_types[ $post_type_name ]->show_in_rest = true;
+		$wp_post_types[ $post_type_name ]->rest_base    = $post_type_name;
+		$wp_post_types[ $post_type_name ]->supports['custom-fields'] = true;
+
+	}
+}
+
+/**
+ * Add REST API support to thirstylink-category taxonomy type.
+ */
+add_action( 'init', 'thirstylink_category_enable_rest_api', 25 );
+function thirstylink_category_enable_rest_api() {
+	global $wp_taxonomies;
+
+	$taxonomy_name = 'thirstylink_category';
+	if ( isset( $wp_taxonomies[ $taxonomy_name ] ) ) {
+		$wp_taxonomies[ $taxonomy_name ]->show_in_rest = true;
+		$wp_taxonomies[ $taxonomy_name ]->rest_base    = $taxonomy_name;
+
+	}
+}
+
+/**
+ * Add REST API support to thirstylink title and thirstyData fields.
+ */
+add_action( 'rest_api_init', function () {
+	register_rest_field( 'thirstylink', 'title', [
+		'get_callback'    => function ( $thirstylink_data ) {
+			$thirstylink_obj = get_post( $thirstylink_data['id'] );
+
+			return $thirstylink_obj->post_title;
+		},
+		'update_callback' => function ( $title, $thirstylink_obj ) {
+			$ret = wp_update_post( [
+				'id'         => $thirstylink_obj->ID,
+				'post_title' => $title,
+			] );
+			if ( false === $ret ) {
+				return new WP_Error( 'rest_thirstylink_title_failed', __( 'Failed to update title.' ),
+					[ 'status' => 500 ] );
+			}
+
+			return true;
+		},
+		'schema'          => [
+			'description' => __( 'Thirstylink title.' ),
+			'type'        => 'string',
+		],
+	] );
+
+	register_rest_field( 'thirstylink', 'thirstyData', [
+		'get_callback'    => function ( $thirstylink_data ) {
+			$thirstylink_data_obj = get_post_meta( $thirstylink_data['id'], 'thirstyData', true );
+
+			return $thirstylink_data_obj;
+		},
+		'update_callback' => function ( $data, $thirstylink_obj ) {
+			$ret = update_post_meta( $thirstylink_obj->ID, 'thirstyData', $data);
+
+			if ( false === $ret ) {
+				return new WP_Error( 'rest_thirstylink_thirstyData_failed', __( 'Failed to update data.' ),
+					[ 'status' => 500 ] );
+			}
+
+			return true;
+		},
+		'schema'          => [
+			'description' => __( 'Thirstylink data.' ),
+			'type'        => 'string',
+		],
+	] );
+} );
